b07a33974d43068503a02b403cbe82bd0f3338a9
CowNation <CowNation.cow@gmail.com>
2019-05-30 08:12:50 -0500

    Fixed crash when lexing empty lines

--- Parser.h
+++ Parser.h
@@ -68,7 +68,7 @@ public:
 	FileLexer(std::vector< std::string > fileLines) {
 		for (int i = 0; i < fileLines.size(); i++) {
 			Lines.push_back(ParseLine(fileLines[i]));
-			if (Lines[i].type[0].type == VariableIdentifier && Lines[i].type[1].type == EqualOperator) {
+			if (Lines[i].type.size() > 1 && Lines[i].type[0].type == VariableIdentifier && Lines[i].type[1].type == EqualOperator) {
 				if (!isVarDefined(Vars, Lines[i].type[0].identifier)) {
 					Vars.push_back(Variable(Lines[i].type[0].identifier, 0));
 					Vars[Vars.size() - 1].Value = Lines[i].Exec(Vars);
