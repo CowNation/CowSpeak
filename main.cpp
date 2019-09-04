0061381da6dc2b61e792ae06af2e6d51e0f5448e
CowNation <CowNation.cow@gmail.com>
2019-09-04 08:52:02 -0500

    Fixed broken includes, make token debugging optional

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "cpp-CowSpeak-master/CowConfig.hpp"
-#include "cpp-CowSpeak-master/Parser.h"
+#include "CowConfig.hpp"
+#include "Parser.h"
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,6 +9,8 @@
 * THIS PROJECT HAS BEEN MOSTLY ABANDONED, IT IS CURRENTLY WORKING BUT WILL RECIEVE FEW UPDATES
 */
 
+#define DEBUG 0
+
 int main() {
 	std::string filename = "Run.COWFILE";
 
@@ -16,15 +18,17 @@ int main() {
 	std::vector< std::string > pLines = cfg.GetLines();
 	FileLexer fl(pLines);
 
-	std::cout << "----------------------------\n";
-	
-	for (int i = 0; i < fl.Lines.size(); i++) {
-		for (int j = 0; j < fl.Lines[i].type.size(); j++) {
-			std::cout << "Line #" << i + 1 << " | Token #" << j + 1 << ": ";
-			PrintTokenType(fl.Lines[i].type[j].type);
-			std::cout << std::endl;
-		}
-		std::cout << "Line #" << i + 1 << " Executed: " << fl.Lines[i].Exec(fl.Vars) << std::endl;
+	if (DEBUG){
 		std::cout << "----------------------------\n";
+
+		for (int i = 0; i < fl.Lines.size(); i++) {
+			for (int j = 0; j < fl.Lines[i].type.size(); j++) {
+				std::cout << "Line #" << i + 1 << " | Token #" << j + 1 << ": ";
+				PrintTokenType(fl.Lines[i].type[j].type);
+				std::cout << std::endl;
+			}
+			std::cout << "Line #" << i + 1 << " Executed: " << fl.Lines[i].Exec(fl.Vars) << std::endl;
+			std::cout << "----------------------------\n";
+		}
 	}
 }
