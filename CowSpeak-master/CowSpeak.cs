75bb324f4caf544ecfa8c2203e6de98605d1d63f
CowNation <CowNation.cow@gmail.com>
2019-10-09 09:01:16 -0500

    Added details for what a function does in it's usage & removed an obsolete member

--- CowSpeak-master/CowSpeak.cs
+++ CowSpeak-master/CowSpeak.cs
@@ -4,26 +4,24 @@ using System;
 namespace CowSpeak{
 	public class CowSpeak{
 		static public List< Function > staticFX = new List< Function >{
-			new Function("exit", Functions.exit, VarType.Void, "exit()"),
-			new Function("pause", Functions.pause, VarType.Void, "pause()"),
-			new Function("clrConsole", Functions.clrConsole, VarType.Void, "clrConsole()"),
-			new Function("clearMem", Functions.clearMem, VarType.Void, "clearMem()"),
-			new Function("inputString", Functions.inputString, VarType.String, "inputString()"),
-			new Function("inputInteger", Functions.inputInt, VarType.Int, "inputInteger()"),
-			new Function("inputDecimal", Functions.inputDecimal, VarType.Decimal, "inputDecimal()"),
-			new Function("print", Functions.print, VarType.Void, "print(string text)", 1),
-			new Function("run", Functions.run, VarType.Void, "run(string fileName)", 1),
-			new Function("randomInteger", Functions.randomInteger, VarType.Int, "randomInteger(integer minimum, integer maximum)", 2),
-			new Function("sleep", Functions.sleep, VarType.Void, "sleep(integer milliseconds)", 1),
+			new Function("exit", Functions.exit, VarType.Void, "exit() - Exits the program"),
+			new Function("pause", Functions.pause, VarType.Void, "pause() - Pauses the program and waits for a keypress"),
+			new Function("clrConsole", Functions.clrConsole, VarType.Void, "clrConsole() - Clears all text from the console"),
+			new Function("clearMem", Functions.clearMem, VarType.Void, "clearMem() - Deletes all variables from memory"),
+			new Function("inputString", Functions.inputString, VarType.String, "inputString() - Allows the user to input a string"),
+			new Function("inputInteger", Functions.inputInt, VarType.Int, "inputInteger() - Allows the user to input a int"),
+			new Function("inputDecimal", Functions.inputDecimal, VarType.Decimal, "inputDecimal() - Allows the user to input a decimal"),
+			new Function("print", Functions.print, VarType.Void, "print(string text) - Prints 'text' to the console", 1),
+			new Function("run", Functions.run, VarType.Void, "run(string fileName) - Executes a cowfile with the name 'fileName'", 1),
+			new Function("randomInteger", Functions.randomInteger, VarType.Int, "randomInteger(integer minimum, integer maximum) - Returns a random integer with a minimum of 'minimum' and a maximum of 'maximum'", 2),
+			new Function("sleep", Functions.sleep, VarType.Void, "sleep(integer milliseconds) - Program waits for 'milliseconds' milliseconds", 1),
 
 			// Comparison Functions
-			new Function("isEqual", Functions.isEqual, VarType.Boolean, "isEqual(left, right)", 2),
-			new Function("isNotEqual", Functions.isNotEqual, VarType.Boolean, "isNotEqual(left, right)", 2),
-			new Function("isLessThan", Functions.isLessThan, VarType.Boolean, "isLessThan(left, right)", 2),
-			new Function("isGreaterThan", Functions.isGreaterThan, VarType.Boolean, "isGreaterThan(left, right)", 2),
+			new Function("isEqual", Functions.isEqual, VarType.Boolean, "isEqual(left, right) - Returns whether left and right are equal", 2),
+			new Function("isNotEqual", Functions.isNotEqual, VarType.Boolean, "isNotEqual(left, right) - Returns whether left and right are not equal", 2),
+			new Function("isLessThan", Functions.isLessThan, VarType.Boolean, "isLessThan(left, right) - Returns whether left is less than right", 2),
+			new Function("isGreaterThan", Functions.isGreaterThan, VarType.Boolean, "isGreaterThan(left, right) - Returns whether left is greater than right", 2),
 		};
-
-		static public List< string[] > Definitions = new List< string[] >();
 
 		static public Function findFunction(string functionName, bool _throw = true){
 			for (int i = 0; i < staticFX.Count; i++){
