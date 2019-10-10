6c5aea238ec4249db4e2d0ec1818c626af15c8ff
CowNation <CowNation.cow@gmail.com>
2019-10-10 16:41:49 -0500

    Removed useless testing code

--- main.cs
+++ main.cs
@@ -4,14 +4,6 @@ using CowSpeak;
 
 class MainClass {
 	public static void Main (string[] args) {
-		List< Token > Tokens = new List< Token >{
-			new Token(TokenType.Number, "5"),
-			new Token(TokenType.AddOperator, "+"),
-			new Token(TokenType.Number, "5"),
-			new Token(TokenType.MultiplyOperator, "*"),
-			new Token(TokenType.Number, "2")
-		};
-		Console.WriteLine(Evaluate.EvaluateTokens(Tokens));
 		CowSpeak.CowSpeak.Exec("main.COWFILE", false);
 	}
 }
