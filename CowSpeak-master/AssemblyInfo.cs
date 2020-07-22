aee7b33ec5dd72e60fc1a1f31479c5c907e149c8
Cow Cow <cownation.cow@gmail.com>
2020-07-22 21:09:53 +0000

    Updated AssemblyVersion

--- CowSpeak-master/AssemblyInfo.cs
+++ CowSpeak-master/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyTitle("CowSpeak")]
 [assembly: AssemblyDescription("An open source interpreted programming language made in C# with a C style syntax")]
 [assembly: AssemblyCompany("CowCorp")]
-[assembly: AssemblyVersion("8")]
+[assembly: AssemblyVersion("8.2")]
